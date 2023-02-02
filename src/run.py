4882948b25e41fa93e57eb7401854010f1db414e
Nikolai Franke <nikolai.franke@gmail.com>
2023-02-02 15:33:55 +0100

    At the end of a run, a gif is saved

--- src/run.py
+++ src/run.py
@@ -12,11 +12,14 @@ from cnn import CNN
 from monitor import OvercookedMonitor
 from overcooked_ai_py.mdp.overcooked_env import OvercookedEnv
 from overcooked_ai_py.mdp.overcooked_mdp import OvercookedGridworld
-from static import MODELS_DIR, RUNS_DIR
+from static import GIFS_DIR, MODELS_DIR, RUNS_DIR
 from tensorboard_callback import TensorboardCallback
+from visualize import save_trajectory_as_gif
 
 
-def make_env(rank, env_id, layout_name, horizon, rew_shaping_params=None, seed=None):
+def make_env(
+    rank, env_id, layout_name, horizon=500, rew_shaping_params=None, seed=None
+):
     def _init():
         mdp = OvercookedGridworld.from_layout_name(
             layout_name, rew_shaping_params=rew_shaping_params
@@ -43,7 +46,7 @@ def make_env(rank, env_id, layout_name, horizon, rew_shaping_params=None, seed=N
 
 
 config_defaults = {
-    "total_timesteps": 7_000_000,
+    "total_timesteps": 5_000_000,
     "batch_size": 1024,
     "learning_rate": 1e-4,
     "horizon": 500,
@@ -57,10 +60,11 @@ config_defaults = {
     "rew_soup_cook": 0,
     "rew_useless_action": 0,
     "rew_wrong_delivery": 0,
-    "layout_name": "simple_o",
+    "layout_name": "asymmetric_advantages",
     "model_name": "ppo",
-    # SAC only config
     "use_sde": False,
+    "n_epochs": 10,
+    # SAC only config
     "learning_starts": 10_000,
 }
 
@@ -80,6 +84,7 @@ def main():
         net_n_neurons = config.net_n_neurons
         model_name = config.model_name
         layout_name = config.layout_name
+        n_epochs = config.n_epochs
 
         # SAC only config
         use_sde = config.use_sde
@@ -130,6 +135,7 @@ def main():
                 policy_kwargs=policy_kwargs,
                 learning_rate=learning_rate,
                 verbose=2,
+                n_epochs=n_epochs,
                 batch_size=batch_size,
                 tensorboard_log=tensorboard_dir,
             )
@@ -155,4 +161,8 @@ def main():
                 WandbCallback(verbose=2),
             ],
         )
+        test_env = make_env(0, env_id, layout_name, horizon, rew_shaping_params)()
+        trajectory = test_env.rollout(model)
+        gif_save_dir = os.path.join(GIFS_DIR, model_save_name)
+        save_trajectory_as_gif(trajectory, gif_save_dir)
         model.save(save_dir)
