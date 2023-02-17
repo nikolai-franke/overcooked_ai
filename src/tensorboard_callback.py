e13b5a10f2c61b1ae4ec79b37c708d91de996238
Nikolai Franke <nikolai.franke@gmail.com>
2023-02-17 13:16:22 +0100

    Fixed tensorboard callback to correctly log the mean instead of only the last values

--- src/tensorboard_callback.py
+++ src/tensorboard_callback.py
@@ -1,4 +1,5 @@
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.utils import safe_mean
 
 
 class TensorboardCallback(BaseCallback):
@@ -6,11 +7,21 @@ class TensorboardCallback(BaseCallback):
         super().__init__(verbose)
 
     def _on_step(self):
-        env_info = self.locals["infos"][0]
-        if "episode" in env_info.keys():
-            episode_info = env_info["episode"]
-            self.logger.record_mean("rollout/ep_useless_a", episode_info["ep_useless_a"])  # type: ignore
-            self.logger.record_mean("rollout/ep_shaped_r", episode_info["ep_shaped_r"])  # type: ignore
-            self.logger.record_mean("rollout/ep_sparse_r", episode_info["ep_sparse_r"])  # type: ignore
-            self.logger.record_mean("rollout/ep_wrong_d", episode_info["ep_wrong_d"])  # type: ignore
+        # env_info = {
+        #     k: [info[k] for info in self.locals["infos"]]
+        #     for k in self.locals["infos"][0]
+        # }
+        env_info = self.locals["infos"]
+        # print(self.locals["infos"])
+        # print(env_info)
+        if "episode" in env_info[0].keys():
+            # e = env_info["episode"]
+            for e in env_info:
+                self.logger.record_mean("rollout/ep_useless_a", e["episode"]["ep_useless_a"])  # type: ignore
+                self.logger.record_mean("rollout/ep_shaped_r", e["episode"]["ep_shaped_r"])  # type: ignore
+                self.logger.record_mean("rollout/ep_sparse_r", e["episode"]["ep_sparse_r"])  # type: ignore
+                self.logger.record_mean("rollout/ep_wrong_d", e["episode"]["ep_wrong_d"])  # type: ignore
+                self.logger.record_mean(
+                    "rollout/ep_collisions", e["episode"]["ep_collisions"]
+                )
         return True
